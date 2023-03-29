1106ae64a95de2f9d65ea71c360e57eb8bdea163
EnriqueRTG <enriqueramontg@gmail.com>
2023-03-29 16:07:55 -0300

    incorporacion de biblioteca de iconos, modificacion en navbar, footer, header y estilos

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,9 +9,9 @@ class Home extends BaseController
     public function index()
     {
         $data = [
-            'title' =>  'Home - Under The Skin',
+            'title' =>  'Principal - Under The Skin',
         ];
 
-        return view('/components/head', $data).view('/components/navbar-cliente', $data).view('/components/carousel', $data).view('/pages/principal').view('/components/footer-cliente');
+        return view('/components/head', $data).view('/components/navbar-cliente', $data).view('/components/carousel', $data).view('/components/header').view('/pages/principal').view('/components/footer-cliente');
     }
 }
